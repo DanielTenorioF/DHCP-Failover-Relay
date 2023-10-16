9a914b8cd77e95c204ca6d477339703b9766892c
Daniel <114906968+DanielTenorioF@users.noreply.github.com>
2023-10-16 14:30:45 +0200

    Update scriptDHCP.sh

--- scriptDHCP.sh
+++ scriptDHCP.sh
@@ -26,3 +26,25 @@ subnet 10.0.0.0 netmask 255.255.0.0 {
 EOF
 
 systemctl restart isc-dhcp-server
+
+systemctl disable NetworkManager
+systemctl stop NetworkManager
+
+cat <<EOF >> /etc/network/interfaces
+# Configuracion red estatica enp0s8
+auto enp0s8
+iface enp0s8 inet static
+address 172.26.2.50
+network 172.26.0.0
+netmask 255.255.0.0
+gateway 172.26.0.1
+
+# Configuracion red estatica enp0s3
+auto enp0s3
+iface enp0s3 inet static
+address 10.0.2.5
+network 10.0.0.0
+netmask 255.255.0.0
+EOF
+
+systemctl restart networking
